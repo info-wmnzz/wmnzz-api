dd097b06cc73c244e5e55f777c2b9f4d2901e8e1
Palanivasan <backend.wmnzz@gmail.com>
2025-12-13 19:22:02 +0530

    google analytics added

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -7,18 +7,27 @@
   <link rel="stylesheet" href="{{ asset('css/style.css') }}">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
   <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;600&display=swap" rel="stylesheet">
-  
+
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id={{ env('GA_MEASUREMENT_ID') }}"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', '{{ env('GA_MEASUREMENT_ID') }}');
+</script>
+
 
 </head>
 <body>
   <div class="logo">
     <img src="{{ asset('landing-page-image/WoomenSquareFlat.png') }}" alt="Logo" />
-    
+
   </div>
   <div class="container">
       <p class="tagline">Wmnzz</p>
       <h1 class="main-heading">COMING SOON</h1>
-     
+
     <div class="social-icons">
       <a href="https://www.linkedin.com/company/wmnzz/" target="_blank" rel="noopener noreferrer"><img src="{{ asset('landing-page-image/linkedin_3536505.png') }}" alt="LinkedIn"></a>
       <a href="https://www.instagram.com/wmnzz.in?igsh=eXRpNXdkd2s1dW15" target="_blank" rel="noopener noreferrer"><img src="{{ asset('landing-page-image/instagram_733558.png') }}" alt="Instagram"></a>
@@ -27,19 +36,19 @@
       <a href="https://linktr.ee/info.wmnzz"target="_blank" rel="noopener noreferrer"><img src="{{ asset('landing-page-image/spotify-logo-png-7057.png') }}" alt="Spotify"></a>
     </div>
     <div id="custom-cursor"></div>
-  
-    <div id="stroke-container"></div> 
+
+    <div id="stroke-container"></div>
     <script>
       const cursor = document.getElementById('custom-cursor');
       const hoverTargets = document.querySelectorAll('.tagline, .main-heading, .social-icons a, .logo img');
-    
+
       document.addEventListener('mousemove', (e) => {
         const x = e.clientX;
         const y = e.clientY;
-    
+
         cursor.style.left = `${x}px`;
         cursor.style.top = `${y}px`;
-    
+
         let hovering = false;
         hoverTargets.forEach((el) => {
           const rect = el.getBoundingClientRect();
@@ -52,18 +61,18 @@
             hovering = true;
           }
         });
-    
+
         document.body.classList.toggle('hovering-text', hovering);
       });
-    
+
       document.addEventListener('mouseleave', () => {
         cursor.style.left = `-150px`;
         cursor.style.top = `-150px`;
         document.body.classList.remove('hovering-text');
       });
     </script>
-    
-    
+
+
   </div>
 </body>
 </html>
